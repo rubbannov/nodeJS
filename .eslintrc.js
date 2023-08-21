9236a722a365dace0f48ab13893e1855fc392f6c
Igor Rubanov <fomagreen@yandex.ru>
2023-08-21 20:14:33 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+    'no-var': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    'no-use-before-define': 'error',
+  },
+}
